f563c66ebd88775ad78fbb175c99647660ff9b77
ShreyaPanday <43707804+ShreyaPanday@users.noreply.github.com>
2020-03-21 00:31:27 +0530

    Update UserController.java

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,4 +1,7 @@
 package controller;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,5 +14,55 @@ import repository.UserRepository;
 public class UserController {
  
 	@Autowired UserRepository userRepo;
-	 
+	  @Autowired
+	   
+
+	    @GetMapping("/User")
+	    public List < User > getAllUsers() {
+	        return userRepo.findAll();
+	    }
+
+	    @GetMapping("/User/{id}")
+	    public ResponseEntity < User > getUsersById(@PathVariable(value = "id") Long UserId)
+	    throws ResourceNotFoundException {
+	        User user = userRepo.findById(UserId)
+	            .orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + UserId));
+	        return ResponseEntity.ok().body(user);
+	    }
+
+	    @PostMapping("/User")
+	    public User createUser(@Valid @RequestBody User user) {
+	        return userRepo.save(user);
+	    }
+
+	    @PutMapping("/User/{id}")
+	    public ResponseEntity < User > updateUser(@PathVariable(value = "id") Long UserId,
+	        @Valid @RequestBody User userDetails) throws ResourceNotFoundException {
+	        User user = userRepo.findById(UserId)
+	            .orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + UserId));
+
+	        
+	        
+	        user.setFirstName(userDetails.getFirstName());
+	        user.setLastName(userDetails.getLastName());
+	        user.setUsername(userDetails.getUserName());
+	        user.setPassword(userDetails.getPassword());
+	        
+	        final User updateduser = userRepo.save(user);
+	        return ResponseEntity.ok(updateduser);
+	    }
+
+	    @DeleteMapping("/User/{id}")
+	    public Map < String, Boolean > deleteUser(@PathVariable(value = "id") Long UserId)
+	    throws ResourceNotFoundException {
+	        User user = userRepo.findById(UserId)
+	            .orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + UserId));
+
+	        userRepo.delete(user);
+	        Map < String, Boolean > response = new HashMap < > ();
+	        response.put("deleted", Boolean.TRUE);
+	        return response;
+	    }
+	}
+}
 }
